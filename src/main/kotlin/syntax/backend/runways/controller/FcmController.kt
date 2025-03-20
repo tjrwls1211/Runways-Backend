b3ac811ee89ddaedaa8553433e423e43bc3c9c3f
tjrwls1211 <seokjin6635@gmail.com>
2025-03-20 16:21:37 +0900

    CHORE : 푸시 알림 DTO 위치 수정

--- src/main/kotlin/syntax/backend/runways/controller/FcmController.kt
+++ src/main/kotlin/syntax/backend/runways/controller/FcmController.kt
@@ -2,15 +2,14 @@ package syntax.backend.runways.controller
 
 import syntax.backend.runways.service.FcmService
 import org.springframework.web.bind.annotation.*
+import syntax.backend.runways.dto.NotificationRequestDTO
 
 @RestController
 @RequestMapping("/api/fcm")
 class FcmController(private val fcmService: FcmService) {
 
-    data class NotificationRequest(val token: String, val title: String, val body: String)
-
     @PostMapping("/send")
-    fun sendNotification(@RequestBody request: NotificationRequest) {
+    fun sendNotification(@RequestBody request: NotificationRequestDTO) {
         fcmService.sendNotification(request.token, request.title, request.body)
     }
 }
