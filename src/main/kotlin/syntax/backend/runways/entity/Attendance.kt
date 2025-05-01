809c115a437459e5469963ba9a8392399e6545e9
tjrwls1211 <seokjin6635@gmail.com>
2025-05-02 00:23:33 +0900

    CHORE : 테이블 명 추가

--- src/main/kotlin/syntax/backend/runways/entity/Attendance.kt
+++ src/main/kotlin/syntax/backend/runways/entity/Attendance.kt
@@ -6,10 +6,12 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import java.time.LocalDate
 import java.util.UUID
 
 @Entity
+@Table(name = "attendance")
 data class Attendance (
     @Id
     @Column(name = "id", updatable = false, nullable = false, columnDefinition = "UUID DEFAULT uuid_generate_v4()")
